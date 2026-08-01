1ed277916519e5a5dac8412cec70e8414fb954a3
Ziyad Khan <2869715@students.wits.ac.za>
2026-08-01 12:03:07 +0200

    added gel growth & changed jump mechanics

--- gel-bounce-pad-prototype/Assets/Scripts/Player/PlayerMovement.cs
+++ gel-bounce-pad-prototype/Assets/Scripts/Player/PlayerMovement.cs
@@ -22,10 +22,10 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
-    public LayerMask groundMask;
+    public LayerMask groundMask, padMask;
 
     Vector3 velocity;
-    bool isGrounded;
+    bool isGrounded, onPad, on2xPad, on3xPad;
 
     private void Awake()
     {
@@ -36,12 +36,14 @@ public class PlayerMovement : MonoBehaviour
         controls.Player.Jump.performed += ctx => jumpHeld = true;
         controls.Player.Jump.canceled += ctx => jumpHeld = false;
         groundMask = LayerMask.GetMask("GelSurface");
+        padMask = LayerMask.GetMask("GelPad");
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        onPad = Physics.CheckSphere(groundCheck.position, groundDistance, padMask);
 
         if (isGrounded && velocity.y < 0)
         {
@@ -62,6 +64,23 @@ public class PlayerMovement : MonoBehaviour
         //check if the player is on the ground so he can jump
 
         if (isGrounded)
+        {
+            if (jumpHeld)
+            {
+                // Charging up
+                isCharging = true;
+            }
+            else if (isCharging)
+            {
+                //float t = chargeTime / maxChargeTime;
+                //float jumpHeight = Mathf.Lerp(minJumpHeight, maxJumpHeight, t);
+                velocity.y = Mathf.Sqrt(minJumpHeight * -2f * gravity);
+
+                isCharging = false;
+                chargeTime = 0f;
+            }
+        }
+        else if(onPad)
         {
             if (jumpHeld)
             {
